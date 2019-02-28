667eebf3d9b15681896c74bd62d40f9495cc6185
Jeffrey Thor <thorjeffrey@gmail.com>
2019-02-27 20:44:50 -0500

    Converts Characters list to a dictionary

--- Characters/ConnectedCharacters.cs
+++ Characters/ConnectedCharacters.cs
@@ -6,7 +6,7 @@ namespace Lerocia.Characters {
   public static class ConnectedCharacters {
     public static User MyUser;
     public static Player MyPlayer;
-    public static List<Character> Characters = new List<Character>();
+    public static Dictionary<int, Character> Characters = new Dictionary<int, Character>();
     public static Dictionary<int, Player> Players = new Dictionary<int, Player>();
     public static Dictionary<int, NPC> NPCs = new Dictionary<int, NPC>();
   }
