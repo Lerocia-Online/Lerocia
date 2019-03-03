ec231f8cdc804aeb62d5da7c6f20b463480ec3d1
Jeffrey Thor <thorjeffrey@gmail.com>
2019-03-03 17:12:15 -0500

    Adds Destination class

---
+++ Characters/NPCs/Destination.cs
@@ -0,0 +1,8 @@
+namespace Lerocia.Characters.NPCs {
+  using UnityEngine;
+
+  public class Destination {
+    public Vector3 Position;
+    public int Duration;
+  }
+}
--- Characters/NPCs/NPC.cs
+++ Characters/NPCs/NPC.cs
@@ -1,43 +1,48 @@
 namespace Lerocia.Characters.NPCs {
   using UnityEngine;
   using System.ComponentModel;
+  using System.Collections.Generic;
 
   public class NPC : Character {
+    public List<Destination> Destinations;
+    
     public NPC() { }
 
     public NPC(
-      int characterId, 
-      string characterName, 
-      string characterPersonality, 
-      GameObject avatar, 
+      int characterId,
+      string characterName,
+      string characterPersonality,
+      GameObject avatar,
       int maxHealth,
-      int currentHealth, 
+      int currentHealth,
       int maxStamina,
-      int currentStamina, 
-      int gold, 
+      int currentStamina,
+      int gold,
       int baseWeight,
-      int baseDamage, 
-      int baseArmor, 
-      int weaponId, 
+      int baseDamage,
+      int baseArmor,
+      int weaponId,
       int apparelId,
       int dialogueId
     ) : base(
-      characterId, 
-      characterName, 
-      characterPersonality, 
-      avatar, 
-      maxHealth, 
-      currentHealth, 
+      characterId,
+      characterName,
+      characterPersonality,
+      avatar,
+      maxHealth,
+      currentHealth,
       maxStamina,
-      currentStamina, 
-      gold, 
+      currentStamina,
+      gold,
       baseWeight,
       baseDamage,
-      baseArmor, 
-      weaponId, 
-      apparelId, 
+      baseArmor,
+      weaponId,
+      apparelId,
       dialogueId
-    ) { }
+    ) {
+      Destinations = new List<Destination>();
+    }
 
     public virtual string[] Interact(string prompt) {
       //TODO Handle NPC interaction
