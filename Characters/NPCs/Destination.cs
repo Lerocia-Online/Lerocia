c3c3611466dcc94acb739d2e601e85bb1222fb39
Jeffrey Thor <thorjeffrey@gmail.com>
2019-03-03 17:15:25 -0500

    Adds Destination constructor

--- Characters/NPCs/Destination.cs
+++ Characters/NPCs/Destination.cs
@@ -4,5 +4,10 @@ namespace Lerocia.Characters.NPCs {
   public class Destination {
     public Vector3 Position;
     public int Duration;
+
+    public Destination(Vector3 position, int duration) {
+      Position = position;
+      Duration = duration;
+    }
   }
 }
