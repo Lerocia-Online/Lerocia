b89bfe51e575c328268e1ac0a9708d19a7fbed8c
Jeffrey Thor <thorjeffrey@gmail.com>
2019-03-15 01:34:23 -0400

    Corrects body name from int to string

--- Characters/Bodies/Body.cs
+++ Characters/Bodies/Body.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 namespace Lerocia.Characters.Bodies {
   public class Body {
     public int BodyId;
-    public int BodyName;
+    public string BodyName;
     public GameObject Avatar;
 
-    public Body(int bodyId, int bodyName, GameObject avatar) {
+    public Body(int bodyId, string bodyName, GameObject avatar) {
       BodyId = bodyId;
       BodyName = bodyName;
       Avatar = avatar;
