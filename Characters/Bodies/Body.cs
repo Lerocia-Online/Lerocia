8824a429464301c29570ab645adba3364247ec61
Jeffrey Thor <thorjeffrey@gmail.com>
2019-03-18 12:02:50 -0400

    Adds inventory to body

--- Characters/Bodies/Body.cs
+++ Characters/Bodies/Body.cs
@@ -1,15 +1,18 @@
-using UnityEngine;
-
 namespace Lerocia.Characters.Bodies {
+  using System.Collections.Generic;
+  using UnityEngine;
+
   public class Body {
     public int BodyId;
     public string BodyName;
     public GameObject Avatar;
+    public List<int> Inventory;
 
-    public Body(int bodyId, string bodyName, GameObject avatar) {
+    public Body(int bodyId, string bodyName, GameObject avatar, List<int> inventory) {
       BodyId = bodyId;
       BodyName = bodyName;
       Avatar = avatar;
+      Inventory = inventory;
     }
   }
 }
